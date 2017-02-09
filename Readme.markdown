bf7d6297ba134739103d1859aa4085ac3f976468
Joakim Gerth <joakim.gerth@gmail.com>
2017-02-09 21:13:11 +0100

    Create example app for Stathat plugin for Corona SDK

---
+++ Readme.markdown
@@ -0,0 +1,40 @@
+# Stathat: Plugin API Docs
+
+## Overview
+
+Example app for [Stathat plugin](https://yogergames.gitlab.io/stathat/) by Yoger Games. It enables Corona SDK users to send and receive stats from [Stathat] (https://www.stathat.com/).
+
+## Features
+Here are a few benefits of using Stathat for analytics and statistics
+
+1. Beautiful charts
+2. Get notified automatically when anomalies occur.
+3. What do the next 30 days look like?
+4. Get an overview of the past hour to the past year, and any other timeframe.
+5. Compare stats to find correlations.
+
+## How to install
+1. Download the files in this repo (https://github.com/yogergames/plugin-stathat/archive/master.zip)
+2. Unpack the zip file
+3. Replace the USER_KEY and ACCESS_TOKEN variables with your data in main.lua
+
+
+## How to use
+Basically the example app provides a few complete examples on the usage of this plugin. 
+
+The following are required though
+
+1. Add the plugin in build settings. 
+2. Require the plugin in your lua source files.
+3. Initialize.
+4. Start sending and receiving stats!
+
+Yep, that's it! Nothing more is required.
+
+PS: __All__ functionality of the plugin is supported on all platforms.
+
+## For more information
+* [API] (https://yogergames.gitlab.io/stathat/api/)
+* [Change log] (https://yogergames.gitlab.io/stathat/changelog/)
+* [Forum Support] (https://forums.coronalabs.com/forum/654-corona-store-plugins/) 
+* {{< email email="support@yogergames.com">}} Email for support {{< /email >}}
---
+++ main.lua
@@ -0,0 +1,236 @@
+-- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
+-- Copyright (C) 2016 Yoger Games AB. All Rights Reserved.
+
+
+------------------------------------------------------------
+-- REPLACE THESE VARIABLES WITH YOUR STATHAT ACCOUNT NAME AND ACCESS TOKEN
+-----------------------------------
+
+local USER_KEY = "hello@example.com"
+local ACCESS_TOKEN = "replace_me"
+
+-----------------------------------
+-- Setup
+-----------------------------------
+local widget = require("widget")
+display.setDefault( "background", 0.9, 0.9, 0.9 )
+local w, h = display.contentWidth,display.contentHeight
+-----------------------------------
+-- Parameters
+-----------------------------------
+local statName = "My stat name"
+local requestId
+local statId
+local chart
+local logo
+local countButton
+local totalCountLabel
+local totalCountText
+local timerId
+local statCountMap = {}
+-----------------------------------
+-- Forward declare functions
+-----------------------------------
+local StathatCallback
+local StathatList
+local StathatInfo
+local StathatData
+local StathatDelete
+local RefreshCountButton
+-----------------------------------
+-- Stathat initialization
+-----------------------------------
+-- Require stathat
+local stathat = require("plugin.stathat")
+
+-- Initialize stathat
+local options = {
+    api = "ez",
+    user_key = USER_KEY,
+    token = ACCESS_TOKEN
+}
+stathat.init(options)
+
+
+-----------------------------------
+-- Stathat callback
+-----------------------------------
+
+local function GetStatIdFromList(list)
+
+    for _,item in next,list,nil do
+        if (item.name == statName) then
+            statId = item.id
+            StathatData()
+            return
+        end
+    end
+end
+
+local function GetStatIdFromInfo(info)
+    statId = info.id
+    StathatData()
+end
+
+local function GetTotalCount(data)
+    local totalCount = 0
+    for _,item in next,data[1].points,nil do
+        totalCount = totalCount + item.value
+    end
+
+    return totalCount
+end
+
+local function GetTotalCountLabel()
+   return ("Total count 1 hour\n(" .. tostring(statName) ..")")
+end
+
+local function GetCountFor(name)
+    if statCountMap[name] then
+        return statCountMap[name]
+    else
+        return 0
+    end
+end
+
+local function RefreshCount(data)
+    local nameOfStat = data[1].name
+    statCountMap[nameOfStat] = GetTotalCount(data)
+
+    if not totalCountLabel then
+        -- Create text label "Total count 1 hour"
+        totalCountLabel = display.newText(GetTotalCountLabel(), 0, 0, native.systemFont, 28 )
+        totalCountLabel.align = "center"
+        totalCountLabel.anchorY = 0
+        totalCountLabel.x = w/2
+        totalCountLabel.y = countButton.y + countButton.height + 50
+        totalCountLabel:setFillColor( 0, 0, 0 )
+    end
+
+    local count = GetCountFor(nameOfStat)
+    if not totalCountText then
+        totalCountText = display.newText(math.round(count*10)*0.1, 0, 0, native.systemFont, 60 )
+        totalCountText.anchorY = 0
+        totalCountText.x = w/2
+        totalCountText.y = totalCountLabel.y + totalCountLabel.height + 10
+        totalCountText:setFillColor( 0, 0, 0 )
+    else
+        totalCountText.text = math.round(count*10)*0.1
+    end
+end
+
+function StathatCallback(event)
+
+    if event.isError then
+        print("Error in request: " .. tostring(event.responseMsg))
+        return
+    end
+
+    if event.responseType == "count" then
+        -- Count was successfully submitted. Let's refresh the chart
+        if not statId then StathatInfo()
+        else StathatData() end
+    elseif event.responseType == "delete" then
+        -- handle succesfully deleted message
+    elseif event.responseType == "list" then
+        GetStatIdFromList(event.payload)
+    elseif event.responseType == "info" then
+        GetStatIdFromInfo(event.payload)
+    elseif event.responseType == "data" then
+        RefreshCount(event.payload)
+
+        -- Start a timer to automatically refresh data, currently updated once a minute
+        timerId = timer.performWithDelay(1000*60, function() StathatData() end)
+    end
+end
+-----------------------------------
+-- Stathat operations
+-----------------------------------
+local function StathatCount()
+    requestId = stathat.count(statName, 1, nil, StathatCallback)
+end
+
+function StathatDelete()
+    stathat.delete(statId, StathatCallback)
+end
+
+function StathatList()
+    stathat.list(StathatCallback)
+end
+
+function StathatInfo()
+    stathat.info(StathatCallback, statName)
+end
+StathatInfo() -- Try to get info for stat immediately
+
+function StathatData()
+    if timerId then
+        timer.cancel(timerId)
+    end
+    stathat.data(statId, nil, "1h", "2m", StathatCallback)
+end
+
+-----------------------------------
+-- User Interface
+-----------------------------------
+function RefreshCountButtonLabel()
+    countButton:setLabel("Send count 1 for \n\"" .. tostring(statName) .. "\"")
+    if totalCountLabel then
+        totalCountLabel.text = GetTotalCountLabel()
+    end
+    if totalCountText then
+        totalCountText.text = math.round(GetCountFor(statName)*10)*0.1
+    end
+    statId = nil
+    StathatInfo(StathatCallback, statName)
+end
+
+-- Logo
+logo = display.newImage("stathat_logo.png", 0,0)
+logo.anchorX, logo.anchorY = 0.5, 0
+local imageRatio = logo.width / logo.height
+logo.width =  0.9 * w
+logo.height = logo.width / imageRatio
+logo.x, logo.y = w/2, 0
+
+-- Input field for stat name
+local textField = native.newTextField( w/2, h/4, w*0.6, 30 )
+textField.anchorY = 0
+textField.y = logo.y + logo.height + 5
+textField:setTextColor( 0.8, 0.8, 0.8 )
+textField.hasBackground = true
+textField.placeholder = statName
+textField.text = statName
+textField:resizeFontToFitHeight()
+textField.text = ""
+textField:addEventListener( "userInput",
+    function(event)
+        if ( event.phase == "submitted" ) then
+        -- Output resulting text from "defaultField"
+            statName = event.target.text
+            RefreshCountButtonLabel()
+            print ("new stat name: " .. statName)
+        end
+    end
+    )
+
+countButton = widget.newButton(
+    {
+        left = 0,
+        top = 0,
+        id = "countButton",
+        onEvent = StathatCount,
+        shape = "roundedRect",
+        width = textField.width,
+        height = 50,
+        cornerRadius = 4,
+        fontSize = 13,
+        fillColor = { default={1,1,1,1}, over={1,1,1,1} },
+        strokeColor = { default={0,0,0,1}, over={0.8,0.8,1,1} },
+        strokeWidth = 3
+    }
+)
+countButton.anchorY = 0
+countButton.x = w/2
+countButton.y = textField.y + textField.height + 15
+RefreshCountButtonLabel()
